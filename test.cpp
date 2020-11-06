d0281cab3e7c7d330ccf0b02dfefb1b46f6b52bf
XiXi1103 <56791578+XiXi1103@users.noreply.github.com>
2020-11-06 20:03:43 +0800

    Update test.cpp

--- test.cpp
+++ test.cpp
@@ -42,11 +42,15 @@ int R(int st,char c){
 			printf("R\n");
 		}
 		else if((stack1[j]=='+'||stack1[j]=='*')&&stack1[j+1]=='N'&&stack1[j-1]=='N'){
+			stack1[top]='\0';
+			stack1[top-1]='\0';
 			top-=2;
 			stack1[top]='N'; 
 			printf("R\n");
 		}
 		else if (stack1[j]==')'&&stack1[j-1]=='N'&&stack1[j-2]=='('){
+			stack1[top]='\0';
+			stack1[top-1]='\0';
 			top-=2;
 			stack1[top]='N';
 			printf("R\n");
@@ -64,7 +68,39 @@ int main(int argc,char *argv[]){
 	FILE *fp;
 	fp = fopen(argv[1], "rb");
 	while(fgets(s,1024,fp)){
-		printf("r");
-		
+//		while(~scanf("%s",s)){
+		int j=0;
+		top=0;
+		stack1[top]='#';
+		int len= strlen(s)-2;
+		s[len]='#';
+		s[len+1]='\0';
+		for(int i=0;i<=len;i++){
+			if(isTerminal(stack1[top])) j=top;
+			else j=top-1;
+			if(cmp(stack1[j],s[i])==1){
+				if(!R(j,s[i])){
+					printf("RE\n");
+					return 0;
+				}
+			}
+			else if(cmp(stack1[j],s[i])==-1){
+				top++;
+				stack1[top]=s[i];
+				printf("I%c\n",stack1[top]);
+			}
+			else if(cmp(stack1[j],s[i])==0){
+				if(s[i]=='#'){
+					return 0;
+				}
+				stack1[++top]=s[i];
+				printf("I%c",stack1[top]);
+			}
+			else{
+				printf("E\n");
+				return 0;
+			}
+		}
+		return 0;
 }
 } 
